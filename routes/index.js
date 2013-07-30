458080e66095c6a32c8333bb1e4788604ce37fde
root <joshamosricher@gmail.com>
2013-07-30 08:27:52 +0400

    Rudimentary and functional index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 spawn = require('child_process').spawn;
 exports.index = function(req,res){
 	console.log("Getting Bacteria")
-	spawn('bash',['shell/listBact.sh']).stdout.on('data',function(data){
+	var ps = spawn('bash',['shell/listBact.sh'])
+	var orgs = []
+	ps.stdout.on('data',function(data){
 		console.log("Got Bacteria")
-		var orgs = data.toString().split("\n")
-		res.render("index",{Organisms:orgs})
+		orgs=orgs.concat(data.toString().split("\n"))
 	})
+	ps.on('close',function(code){
+		res.render("index",{Organisms:orgs})
+})
 }
